53c44fc569f0954f32f79251477ccae3ca5c4927
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-06-05 12:19:15 +0530

    fixed all bugs and add useRef when submit form field all clear

--- AnkusamApp/src/components/VehiclePages/PostVehicleAvailability/PostVehicleAvailability.jsx
+++ AnkusamApp/src/components/VehiclePages/PostVehicleAvailability/PostVehicleAvailability.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { City, State } from "country-state-city";
 import Select from "react-select";
 import { weightdata } from "../../../data/WeightData";
@@ -63,6 +63,8 @@ function PostVehicleAvailability() {
   const handleFromStateChange = (selectedOption) => {
     setFromStateCode(selectedOption ? selectedOption.value : "");
     setFromStateName(selectedOption ? selectedOption.label : "");
+    // Reset state selection when "from" state changes
+    setFromCityName("");
   };
 
   // Handle city change for "from"
@@ -75,6 +77,8 @@ function PostVehicleAvailability() {
   const handleToStateChange = (selectedOption) => {
     setToStateCode(selectedOption ? selectedOption.value : "");
     setToStateName(selectedOption ? selectedOption.label : "");
+    // Reset state selection when "To" state changes
+    setToCityName("");
   };
 
   // Handle city change for "to"
@@ -102,11 +106,26 @@ function PostVehicleAvailability() {
     label: city.name,
   }));
 
+  // ==============👇 Form submit control Section 👇=================
+  
+  //👉Reset form Or form fields submit after clear all fields
+  const formResetUseingRef = useRef();
+  const fromStateInputRef = useRef();
+  const fromCityInputRef = useRef();
+  const toStateInputRef = useRef();
+  const toCityInputRef = useRef();
+
   // State for form data and loading status
   const [isLoading, setIsLoading] = useState(false);
 
+  // Error handling state
+  const [isErrors, setIsErrors] = useState({
+    fromCityName: "",
+    toCityName: "",
+  });
+
   const [files, setFiles] = useState({
-    typeOfVehicle: null,
+    typeOfVehicle: "Both",
     vehicle_capacity_in_tons: null,
     vehicleLength: null,
   });
@@ -123,6 +142,36 @@ function PostVehicleAvailability() {
   // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!fromCityName) {
+      toast("Current City is required!");
+      setIsErrors((prevErrors) => ({
+        ...prevErrors,
+        fromCityName: "Current City is required!",
+      }));
+      return;
+    } else {
+      setIsErrors((prevErrors) => ({
+        ...prevErrors,
+        fromCityName: "",
+      }));
+    }
+
+    if (!toCityName) {
+      toast("Destination City is required!");
+      setIsErrors((prevErrors) => ({
+        ...prevErrors,
+        toCityName: "Destination City is required!",
+      }));
+      return;
+    } else {
+      setIsErrors((prevErrors) => ({
+        ...prevErrors,
+        fromCityName: "",
+        toCityName: "",
+      }));
+    }
+
     setIsLoading(true);
 
     const formData = new FormData();
@@ -158,10 +207,12 @@ function PostVehicleAvailability() {
           theme: "colored",
         });
 
-        setTimeout(() => {
-          // Refresh the page
-          window.location.reload();
-        }, 1500);
+        //👉Reset form Or form fields submit after clear all fields
+        formResetUseingRef.current.reset();
+        fromStateInputRef.current.clearValue();
+        fromCityInputRef.current.clearValue();
+        toStateInputRef.current.clearValue();
+        toCityInputRef.current.clearValue();
 
       } else {
         setIsLoading(false);
@@ -189,6 +240,16 @@ function PostVehicleAvailability() {
         theme: "colored",
       });
     }
+
+    console.log("====================================");
+    console.log("FromState: " + fromStateName);
+    console.log("FromCity: " + fromCityName);
+    console.log("ToState: " + toStateName);
+    console.log("ToCity: " + toCityName);
+    console.log("typeOfVehicle: " + files.typeOfVehicle);
+    console.log("vehicle_capacity_in_tons: " + files.vehicle_capacity_in_tons);
+    console.log("vehicleLength: " + files.vehicleLength);
+    console.log("====================================");
   };
 
   return (
@@ -205,164 +266,160 @@ function PostVehicleAvailability() {
       </div>
 
       {/* Shipping Post form */}
-      <div className="md:mt-36 mt-10 py-6 pt-10 px-6 border md:w-[80%] w-[95%] mx-auto bg-[#f4f4f4] rounded-lg shadow-md shadow-gray-700">
-        <form onSubmit={handleSubmit}>
-          {/* Shipping from part */}
+      <div className="md:mt-36 mt-24 py-6 pt-10 px-6 border md:w-[80%] w-[95%] mx-auto bg-[#f4f4f4] rounded-lg shadow-md shadow-gray-700">
+        <form onSubmit={handleSubmit} ref={formResetUseingRef}>
+          {/* Current Location or from state part */}
           <div className="w-full mx-auto grid lg:grid-cols-2">
             <div className="text-[#66451C]">
-              <div>
-                <h2 className="lg:text-4xl md:text-3xl text-[22px]">
-                  Where are you shipping from?
-                </h2>
-                <div className="grid grid-cols-2 mt-4">
-                  <div>
-                    <h3 className="md:text-lg font-semibold text-black">
-                      SELECT STATE
-                    </h3>
-                    <Select
-                      name="fromState"
-                      id="fromState"
-                      className="cursor-pointer w-[130px]"
-                      required
-                      value={stateOptions.find(
-                        (option) => option.value === fromStateCode
-                      )}
-                      onChange={handleFromStateChange}
-                      options={stateOptions}
-                    />
-                  </div>
-                  <div>
-                    <h3 className="md:text-lg font-semibold text-black">
-                      SELECT CITY
-                    </h3>
-                    <Select
-                      name="fromCity"
-                      id="fromCity"
-                      required
-                      className="cursor-pointer w-[150px]"
-                      value={fromCityOptions.find(
-                        (option) => option.value === fromCity
-                      )}
-                      onChange={handleFromCityChange}
-                      options={fromCityOptions}
-                      isDisabled={!fromStateCode}
-                    />
-                  </div>
-                </div>
-              </div>
-
-              
-                <h1 className="text-3xl">Here add current location and this data show on business available load section</h1>
-
-
-              {/* <div className="md:mt-20 mt-10">
-                <h2 className="lg:text-4xl md:text-3xl text-[22px]">
-                  How would you like to deliver?
-                </h2>
-                <div className="mt-4">
+              <h2 className="lg:text-4xl text-2xl">Current Location</h2>
+              <div className="grid md:grid-cols-2 mt-4">
+                <div>
                   <h3 className="md:text-lg font-semibold text-black">
-                    TYPE OF VEHICLE
+                    SELECT STATE
                   </h3>
-                  <select
-                    name="typeOfVehicle"
-                    id="vehicleType"
-                    className="py-2 px-4 md:w-[60%] border outline-none rounded-lg shadow-md cursor-pointer"
-                    onChange={handleChange}
+                  <Select
+                    ref={fromStateInputRef}
+                    name="fromState"
+                    id="fromState"
+                    className="cursor-pointer lg:w-[180px] w-[90%]"
                     required
-                  >
-                    <option value="">Select one..</option>
-                    <option value="Open Body">Open Body</option>
-                    <option value="Closed Body">Closed Body</option>
-                  </select>
-                </div>
-              </div> */}
-            </div>
-
-            {/* Shipping To part */}
-            <div className="text-[#66451C] mt-4">
-              <div>
-                <h2 className="lg:text-4xl md:text-3xl text-[22px]">
-                  Where are you shipping to?
-                </h2>
-                <div className="grid grid-cols-2 mt-4">
-                  <div>
-                    <h3 className="md:text-lg font-semibold text-black">
-                      SELECT STATE
-                    </h3>
-                    <Select
-                      name="toState"
-                      id="toState"
-                      required
-                      className="cursor-pointer w-[130px]"
-                      value={stateOptions.find(
-                        (option) => option.value === toStateCode
-                      )}
-                      onChange={handleToStateChange}
-                      options={stateOptions}
-                    />
-                  </div>
-                  <div>
-                    <h3 className="md:text-lg font-semibold text-black">
-                      SELECT CITY
-                    </h3>
-                    <Select
-                      name="toCity"
-                      id="toCity"
-                      required
-                      className="cursor-pointer w-[150px]"
-                      value={toCityOptions.find(
-                        (option) => option.value === toCity
-                      )}
-                      onChange={handleToCityChange}
-                      options={toCityOptions}
-                      isDisabled={!toStateCode}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="md:mt-20 mt-8">
-                <h2 className="lg:text-4xl md:text-3xl text-[22px]">
-                  Vehicle Details
-                </h2>
-                <div className="mt-4">
-                  <h3 className="md:text-lg font-semibold text-black">
-                    Available Capacity in Tons
-                  </h3>
-                  <select
-                    name="vehicle_capacity_in_tons"
-                    id="vehicleCapacity"
-                    className="py-2 px-4 md:w-[60%] border outline-none rounded-lg shadow-md cursor-pointer"
-                    onChange={handleChange}
-                    required
-                  >
-                    <option value="">Select capacity</option>
-                    {weightdata.map((capacity, index) => (
-                      <option key={index} value={capacity}>
-                        {capacity}
-                      </option>
-                    ))}
-                  </select>
-                  <br />
-                  <h3 className="md:text-lg font-semibold text-black">
-                    Vehicle Length
-                  </h3>
-                  <input
-                    className="py-2 px-4 md:w-[60%] border outline-none rounded-lg shadow-md"
-                    type="text"
-                    name="vehicleLength"
-                    placeholder="Please enter vehicle length.."
-                    required
-                    onChange={handleChange}
+                    value={stateOptions.find(
+                      (option) => option.value === fromStateCode
+                    )}
+                    onChange={handleFromStateChange}
+                    options={stateOptions}
                   />
                 </div>
+                <div className="md:mt-0 mt-3">
+                  <h3 className="md:text-lg font-semibold text-black">
+                    SELECT CITY
+                  </h3>
+                  <Select
+                    ref={fromCityInputRef}
+                    name="fromCity"
+                    id="fromCity"
+                    required
+                    className="cursor-pointer lg:w-[180px] w-[90%]"
+                    value={fromCityOptions.find(
+                      (option) => option.value === fromCity
+                    )}
+                    onChange={handleFromCityChange}
+                    options={fromCityOptions}
+                    isDisabled={!fromStateCode}
+                  />
+                  {isErrors && (
+                    <p className="text-red-500">{isErrors.fromCityName}</p>
+                  )}
+                </div>
               </div>
+
+              {/* <h1 className="text-3xl">Here add current location and this data show on business available load section</h1> */}
+            </div>
+
+            {/* Destination Location part */}
+            <div className="text-[#66451C]">
+              {/* <div> */}
+              <h2 className="lg:text-4xl text-2xl">Destination Location</h2>
+              <div className="grid md:grid-cols-2 mt-4">
+                <div>
+                  <h3 className="md:text-lg font-semibold text-black">
+                    SELECT STATE
+                  </h3>
+                  <Select
+                    ref={toStateInputRef}
+                    name="toState"
+                    id="toState"
+                    required
+                    className="cursor-pointer lg:w-[180px] w-[90%]"
+                    value={stateOptions.find(
+                      (option) => option.value === toStateCode
+                    )}
+                    onChange={handleToStateChange}
+                    options={stateOptions}
+                  />
+                </div>
+                <div className="md:mt-0 mt-3">
+                  <h3 className="md:text-lg font-semibold text-black">
+                    SELECT CITY
+                  </h3>
+                  <Select
+                    ref={toCityInputRef}
+                    name="toCity"
+                    id="toCity"
+                    required
+                    className="cursor-pointer lg:w-[180px] w-[90%]"
+                    value={toCityOptions.find(
+                      (option) => option.value === toCity
+                    )}
+                    onChange={handleToCityChange}
+                    options={toCityOptions}
+                    isDisabled={!toStateCode}
+                  />
+                  {isErrors && (
+                    <p className="text-red-500">{isErrors.toCityName}</p>
+                  )}
+                </div>
+              </div>
+              {/* </div> */}
+            </div>
+          </div>
+
+          {/* Vehicle Details part */}
+          <div className="md:mt-20 mt-5 text-[#66451C]">
+            <div>
+              <h2 className="md:text-4xl text-2xl">Vehicle Details</h2>
+              <div className="mt-4">
+                <h3 className="md:text-lg font-semibold text-black">
+                  Available Capacity in Tons
+                </h3>
+                <select
+                  name="vehicle_capacity_in_tons"
+                  id="vehicleCapacity"
+                  className="py-2 px-4 md:w-[60%] w-[90%] min-w-[210px] border outline-none rounded-lg shadow-md cursor-pointer"
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select capacity</option>
+                  {weightdata.map((capacity, index) => (
+                    <option key={index} value={capacity}>
+                      {capacity}
+                    </option>
+                  ))}
+                </select>
+                <br />
+                <h3 className="md:text-lg font-semibold text-black md:mt-0 mt-3">
+                  Vehicle Length
+                </h3>
+                <input
+                  className="py-2 px-4 md:w-[60%] w-[90%] min-w-[210px] border outline-none rounded-lg shadow-md"
+                  type="text"
+                  name="vehicleLength"
+                  placeholder="Please enter vehicle length.."
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <h3 className="md:text-lg font-semibold text-black md:mt-0 mt-3">
+                Type of Vehicle
+              </h3>
+              <select
+                name="typeOfVehicle"
+                id="typeOfVehicle"
+                className="py-2 px-4 md:w-[60%] w-[90%] text-black min-w-[210px] border outline-none rounded-lg shadow-md cursor-pointer"
+                onChange={handleChange}
+                required
+              >
+                <option value="Both">Both</option>
+                <option value="Close Body">Close Body</option>
+                <option value="Open Body">Open Body</option>
+              </select>
             </div>
           </div>
           <div className="w-[80%] mx-auto text-center">
-            <button
-              type="submit"
-              className="w-[120px] my-10 text-white py-3 ml-auto rounded-lg text-2xl shadow-md shadow-[yellow] font-bold bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500"
-            >
+            <button className="w-[120px] my-10 text-white py-3 ml-auto rounded-lg text-2xl shadow-md shadow-[yellow] font-bold bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">
               Submit
             </button>
           </div>
