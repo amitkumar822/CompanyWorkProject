786fbfedf83fe524f74126adccf2063e748a3d44
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-06-23 10:13:56 +0530

    complete Invoice

--- AnkusamInvoice/src/components/InvoiceForm.jsx
+++ AnkusamInvoice/src/components/InvoiceForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AnkusamLogo from "../Photos/AnkusamLogo.png";
 import TUVLogo from "../Photos/TUVLogo.png";
 import jsPDF from "jspdf";
@@ -28,28 +28,57 @@ const InvoiceForm = () => {
     );
   };
 
+  const [texableValue, setTexableValue] = useState(0);
+
   return (
     <>
-      <div className="w-[800px] mx-auto mt-4">
+      {/*============👇 Print Section 👇========== */}
+
+      <div className="flex justify-center mt-4 no-print">
+        <button
+          onClick={handlePrint}
+          className="mr-4 px-4 bg-blue-500 text-white py-2 rounded"
+        >
+          Print
+        </button>
+        {/* <button
+          onClick={handleDownloadPDF}
+          className="bg-green-500 text-white py-2 rounded"
+        >
+          Download PDF
+        </button> */}
+      </div>
+
+      <div className="lg:w-[80%] w-[800px] mx-auto mt-4">
         <div
           className="w-full mx-auto py-2 print-border border-black border-[1px]"
           id="invoice"
         >
           {/* Company All Details or Header section */}
           <div className="px-3">
-            <h1 className="font-bold text-center italic">Purchase Order</h1>
+            <h1 className="font-bold text-center italic lg:text-[20px]">
+              Purchase Order
+            </h1>
             <div className="flex justify-around items-center px-4">
-              <img src={AnkusamLogo} className="w-[50px]" alt="Ankusam Logo" />
-              <h1 className="font-bold italic font-serif text-2xl">
+              <img
+                src={AnkusamLogo}
+                className="lg:w-[90px] w-[50px]"
+                alt="Ankusam Logo"
+              />
+              <h1 className="font-bold italic font-serif lg:text-4xl text-2xl">
                 Ankusam Engineering Private Limited
               </h1>
               <span className="flex flex-col items-center">
-                <img src={TUVLogo} className="w-[30px]" alt="TUV Logo" />
+                <img
+                  src={TUVLogo}
+                  className="lg:w-[50px] w-[30px]"
+                  alt="TUV Logo"
+                />
                 <h1 className="font-semibold text-[10px]">ISO 90001:2015</h1>
               </span>
             </div>
             {/* Address */}
-            <div className="text-center text-[11.2px]">
+            <div className="text-center text-[11.2px] lg:text-[17px]">
               <h1>
                 3/204 E2, Venkittapuram, Near L&T By-pass Road, Coimbatore,
                 Tamil Nadu - 641062
@@ -60,100 +89,100 @@ const InvoiceForm = () => {
               <h1>Contact Number : +91 7305046742, 7305046744, 9003441337</h1>
               <h1>GSTIN: 33AAUCA1961FlZN</h1>
             </div>
-            <span className="text-[10px]">TO</span>
+            <span className="text-[10px] lg:text-[18px]">TO</span>
           </div>
 
           {/* Builling section */}
-          <div className="text-[11px]">
+          <div className="text-[11px] lg:text-[19px]">
             {/* Name and date section */}
             <div className=" border-t-[1px] border-black flex">
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center">
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold">
                 Name
               </div>
-              <div className="w-[449.5px] pl-1 border-r-[1px] border-black font-semibold">
+              <div className="w-[448px] lg:w-[650px] pl-1 border-r-[1px] border-black">
                 Sri Kumaran Steels
               </div>
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center">
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold">
                 Date:
               </div>
-              <div className="w-[148px] pl-1 font-semibold flex items-center">
+              <div className="w-[148px] lg:w-[226px] pl-1 flex items-center">
                 20.11.2024
               </div>
             </div>
 
             {/* Address section */}
             <div className=" border-t-[1px] border-b-[1px] border-black flex">
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center">
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold">
                 Address
               </div>
-              <div className="w-[449.5px] pl-1 border-r-[1px] border-black font-semibold">
+              <div className="w-[448px] lg:w-[650px] pl-1 border-r-[1px] border-black">
                 3/204 E2, Venkittapuram, Near L&T By-pass Road, Coimbatore,3/204
                 E2, Venkittapuram, Near L&T By-pass Road, Coimbatore, Tamil Nadu
                 - 641062 Tamil Nadu - 641062
               </div>
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center">
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold">
                 No:
               </div>
-              <div className="w-[148px] pl-1 font-semibold flex items-center">
+              <div className="w-[148px] lg:w-[226px] pl-1 flex items-center">
                 No: AEPL/PO/24-25/37
               </div>
             </div>
 
             {/* State and Supplier section */}
             <div className=" border-b-[1px] border-black flex">
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center">
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold">
                 State
               </div>
-              <div className="w-[449.5px] pl-1 border-r-[1px] border-black font-semibold">
+              <div className="w-[448px] lg:w-[650px] pl-1 border-r-[1px] border-black">
                 Tamil Nadu
               </div>
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center">
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold">
                 Supplier code:
               </div>
-              <div className="w-[148px] pl-1 font-semibold flex items-center">
+              <div className="w-[148px] lg:w-[226px] pl-1 flex items-center">
                 12345
               </div>
             </div>
 
             {/* Mobile and Delivery date section */}
             <div className=" border-b-[1px] border-black flex">
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center">
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold">
                 Mobile
               </div>
-              <div className="w-[449.5px] pl-1 border-r-[1px] border-black font-semibold">
+              <div className="w-[448px] lg:w-[650px] pl-1 border-r-[1px] border-black">
                 1234567890
               </div>
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center">
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold">
                 Date of delivery:
               </div>
-              <div className="w-[148px] pl-1 font-semibold flex items-center">
+              <div className="w-[148px] lg:w-[226px] pl-1 flex items-center">
                 15.12.2024
               </div>
             </div>
 
             {/* GST number section */}
             <div className=" border-b-[1px] border-black flex">
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center">
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold">
                 GST no:
               </div>
-              <div className="w-[449.5px] pl-1 border-r-[1px] border-black font-semibold">
+              <div className="w-[448px] lg:w-[650px] pl-1 border-r-[1px] border-black">
                 33AXYZ3235PQR34
               </div>
-              <div className="w-[90.3px] pl-1 border-r-[1px] border-black flex items-center"></div>
-              <div className="w-[148px] pl-1 font-semibold flex items-center"></div>
+              <div className="w-[91px] lg:w-[150px] pl-1 border-r-[1px] border-black flex items-center font-semibold"></div>
+              <div className="w-[148px] lg:w-[226px] pl-1 flex items-center"></div>
             </div>
 
             {/* Description of Goods Table Section */}
-            <table>
+            <table className="lg:w-full">
               <thead className="bg-gray-300">
-                <tr className="border-b-[1px] border-black">
-                  <th className="border-r-[1px] border-black py-2 w-[90.3px]">
+                <tr className="border-b-[1px] border-black lg:text-[18px]">
+                  <th className="border-r-[1px] border-black py-2 w-[91px] lg:w-[104px]">
                     S.No.
                   </th>
                   <th className="border-r-[1px] border-black py-2 w-[381px]">
                     Description of Goods
                   </th>
-                  <th className="border-r-[1px] border-black py-2 w-[68.2px]">
+                  <th className="border-r-[1px] border-black py-2 w-[67.2px] lg:w-[65px]">
                     HSN Code
                   </th>
                   <th className="border-r-[1px] border-black py-2 w-[90.5px]">
@@ -167,22 +196,24 @@ const InvoiceForm = () => {
                   </th>
                 </tr>
               </thead>
-              <tbody className=" font-normal text-center text-[13px]">
+              <tbody className=" font-normal text-center text-[13px] lg:text-[18px]">
                 {AllDtata.map((data, index) => (
                   <tr className="border-b-[1px] border-black" key={index}>
-                    <td className="border-r-[1px] border-black py-2 w-[90.3px]">
+                    <td className="border-r-[1px] border-black py-2 w-[91px] lg:w-[104px]">
                       {index + 1}
                     </td>
                     <td className="border-r-[1px] border-black py-2 w-[381px]">
                       {data.goods}
                     </td>
-                    <td className="border-r-[1px] border-black py-2 w-[68.2px]">
+                    <td className="border-r-[1px] border-black py-2 w-[68.2px] lg:w-[65px]">
                       {data.hsnCode}
                     </td>
                     <td className="border-r-[1px] border-black py-2 w-[90.5px]">
                       {data.qty}
                     </td>
-                    <td className="border-r-[1px] border-black py-2 w-[60px]"></td>
+                    <td className="border-r-[1px] border-black py-2 w-[60px]">
+                      {data.rate}
+                    </td>
                     <td className="border-black py-2 w-[109.1px]"></td>
                   </tr>
                 ))}
@@ -191,60 +222,60 @@ const InvoiceForm = () => {
 
             {/* CGST and SGST Section */}
             {/* Total */}
-            <div className=" border-black flex">
-              <div className="w-[90.4px] border-r-[1px] border-black"></div>
-              <div className="w-[381px] border-r-[1px] border-black"></div>
-              <div className="w-[68px] border-r-[1px] border-black"></div>
-              <div className="w-[90.7px] border-r-[1px] border-black text-center font-semibold text-[13px]">
+            <div className=" border-black flex lg:text-[18px] text-[13px]">
+              <div className="w-[91px] lg:w-[152px] border-r-[1px] border-black"></div>
+              <div className="w-[380px] lg:w-[553.5px] border-r-[1px] border-black"></div>
+              <div className="w-[68px] lg:w-[94.5px] border-r-[1px] border-black"></div>
+              <div className="w-[91px] lg:w-[131px] border-r-[1px] border-black text-center font-semibold">
                 Total
               </div>
-              <div className="w-[60.2px] border-r-[1px] border-black text-center"></div>
-              <div className="w-[108.8px] border-black text-center"></div>
+              <div className="w-[60.2px] lg:w-[87px] border-r-[1px] border-black text-center"></div>
+              <div className="w-[107px] lg:w-[158.4px] border-black text-center"></div>
             </div>
 
             {/* CGST */}
-            <div className="border-black flex">
-              <div className="w-[90.4px] border-r-[1px] border-black"></div>
-              <div className="w-[381px] border-r-[1px] border-black"></div>
-              <div className="w-[68px] border-r-[1px] border-black"></div>
-              <div className="w-[90.7px] border-r-[1px] border-black text-center font-semibold">
+            <div className=" border-black flex lg:text-[18px] text-[13px]">
+              <div className="w-[91px] lg:w-[152px] border-r-[1px] border-black"></div>
+              <div className="w-[380px] lg:w-[553.5px] border-r-[1px] border-black"></div>
+              <div className="w-[68px] lg:w-[94.5px] border-r-[1px] border-black"></div>
+              <div className="w-[91px] lg:w-[131px] border-r-[1px] border-black text-center font-semibold">
                 CGST
               </div>
-              <div className="w-[60.2px] border-r-[1px] border-black text-center">
+              <div className="w-[60.2px] lg:w-[87px] border-r-[1px] border-black text-center">
                 9%
               </div>
-              <div className="w-[108.8px] border-black text-center">9%</div>
+              <div className="w-[107px] lg:w-[158.4px] border-black text-center">9%</div>
             </div>
 
             {/* SGST */}
             <div className=" border-b-[1px] border-black flex">
-              <div className="w-[90.4px] border-r-[1px] border-black"></div>
-              <div className="w-[381px] border-r-[1px] border-black"></div>
-              <div className="w-[68px] border-r-[1px] border-black"></div>
-              <div className="w-[90.7px] border-r-[1px] border-black text-center font-semibold">
+              <div className="w-[91px] lg:w-[152px] border-r-[1px] border-black"></div>
+              <div className="w-[380px] lg:w-[553.5px] border-r-[1px] border-black"></div>
+              <div className="w-[68px] lg:w-[94.5px] border-r-[1px] border-black"></div>
+              <div className="w-[91px] lg:w-[131px] border-r-[1px] border-black text-center font-semibold">
                 SGST <br />
                 Rounded off
               </div>
-              <div className="w-[60.2px] border-r-[1px] border-black text-center">
+              <div className="w-[60.2px] lg:w-[87px] border-r-[1px] border-black text-center">
                 9%
               </div>
-              <div className="w-[108.8px] border-black text-center">9%</div>
+              <div className="w-[107px] lg:w-[158.4px] border-black text-center">9%</div>
             </div>
-            
+
             {/* Grand Total */}
             <div className=" border-b-[1px] border-black flex">
-              <div className="w-[90.4px] border-r-[1px] border-black"></div>
-              <div className="w-[381px] border-r-[1px] border-black"></div>
-              <div className="w-[68px] border-r-[1px] border-black"></div>
-              <div className="w-[151px] border-r-[1px] border-black text-center font-semibold">
+              <div className="w-[91px] lg:w-[152px] border-r-[1px] border-black"></div>
+              <div className="w-[380px] lg:w-[553.5px] border-r-[1px] border-black"></div>
+              <div className="w-[68px] lg:w-[94.5px] border-r-[1px] border-black"></div>
+              <div className="w-[151px] lg:w-[218px] border-r-[1px] border-black text-center font-semibold">
                 Grand Total
               </div>
-              <div className="w-[108.8px] border-black text-center">12056</div>
+              <div className="w-[107px] lg:w-[158.4px] border-black text-center">12056</div>
             </div>
           </div>
 
           {/* Terms and conditions:- */}
-          <div className="text-[11px] border-b-[1px] pl-2 border-black">
+          <div className="text-[11px] lg:text-[18px] border-b-[1px] pl-2 border-black">
             <h1 className=" font-semibold">Terms and conditions:-</h1>
             <ul className="pl-8 ">
               {TermsAndConditions.map((term, index) => (
@@ -256,7 +287,7 @@ const InvoiceForm = () => {
           </div>
 
           {/* Thanking you section */}
-          <div className="text-[9px] pl-4">
+          <div className="text-[9px] lg:text-[18px] pl-4">
             <h3>Thanking you,</h3>
             <h1 className=" font-semibold">
               For Ankusam Engineering Private Limited,
@@ -265,23 +296,7 @@ const InvoiceForm = () => {
             <h2 className="font-semibold">Managing Director</h2>
           </div>
         </div>
-
-        {/*============👇 Print Section 👇========== */}
-
-        <div className="flex justify-center mt-4 no-print">
-          <button
-            onClick={handlePrint}
-            className="mr-4 bg-blue-500 text-white py-2 rounded"
-          >
-            Print
-          </button>
-          <button
-            onClick={handleDownloadPDF}
-            className="bg-green-500 text-white py-2 rounded"
-          >
-            Download PDF
-          </button>
-        </div>
+        <div className="lg:mt-6"></div>
       </div>
     </>
   );
@@ -338,42 +353,28 @@ const AllDtata = [
     si: 1,
     goods: "40x40x1.6 mm Thickness MS & Sq Tube",
     hsnCode: "23",
-    qty: 10,
+    qty: 3,
+    rate: 120,
   },
   {
     si: 2,
     goods: "50x60x1.7 mm Thickness MS & Sq Tube",
     hsnCode: "23",
-    qty: 5,
+    qty: 4,
+    rate: 50,
   },
   {
     si: 2,
     goods: "50x60x1.7 mm Thickness MS & Sq Tube",
     hsnCode: "23",
-    qty: 5,
+    qty: 6,
+    rate: 60,
   },
   {
     si: 2,
     goods: "50x60x1.7 mm Thickness MS & Sq Tube",
     hsnCode: "23",
-    qty: 5,
-  },
-  {
-    si: 2,
-    goods: "50x60x1.7 mm Thickness MS & Sq Tube",
-    hsnCode: "23",
-    qty: 5,
-  },
-  {
-    si: 2,
-    goods: "50x60x1.7 mm Thickness MS & Sq Tube",
-    hsnCode: "23",
-    qty: 5,
-  },
-  {
-    si: 2,
-    goods: "50x60x1.7 mm Thickness MS & Sq Tube",
-    hsnCode: "23",
-    qty: 5,
+    qty: 8,
+    rate: 40,
   },
 ];
