44bebc4bc3b49c7cd36f3f19a4088877a61cb99c
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-07-05 10:04:45 +0530

    add background animation in login page and setup vite config also index.css set animation css

--- ShopAdmin/vite.config.js
+++ ShopAdmin/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  darkMode: 'class', // background animations configuration
   plugins: [react()],
   server: {
     proxy: {
