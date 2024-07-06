f8fd8f14ad6ac9d935957c71953414629e6e162e
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-07-06 11:35:36 +0530

    create new element rejected page

---
+++ ShopAdmin/src/components/PO_Pages/RejectedPOPages/RejectedPo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function RejectedPo() {
+  return (
+    <>
+      <div className="mt-16">
+        <div className="w-[80%] mx-auto">
+          <h1 className="text-4xl text-center mt-20 font-semibold underline">
+            Rejected Positions Page
+          </h1>
+          <p className="w-[70%] mx-auto text-xl text-center">
+            Here you can find the latest rejected positions in our company.
+            Please, check the details and make necessary corrections.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default RejectedPo;
