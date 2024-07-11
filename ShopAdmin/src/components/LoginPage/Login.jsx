3aa067a2706733732a4508b91635a1c9336e0d41
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-07-11 14:58:06 +0530

    fix error

--- ShopAdmin/src/components/LoginPage/Login.jsx
+++ ShopAdmin/src/components/LoginPage/Login.jsx
@@ -123,8 +123,8 @@ function Login() {
         </div>
         <div className="w-[80%] h-full mx-auto flex justify-center items-center">
           <div className="w-[500px] mx-auto bg-gradient-to-r from-fuchsia-500 to-cyan-500 text-white rounded-lg shadow-md shadow-[yellow] py-4 px-2">
-            <h1 className="text-center text-3xl font-bold italic font-serif underline">
-              Login Page
+            <h1 className="text-center text-3xl font-bold italic font-serif underline ">
+              Login
             </h1>
             <form
               action=""
